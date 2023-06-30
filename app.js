d0918939cc9a15d7864b2c04f1c74e3ce5071b09
anoy21 <thienhung.kp2@gmail.com>
2023-06-30 20:04:46 +0700

    latest

---
+++ app.js
@@ -0,0 +1,18 @@
+// const api_url1 = 
+// "https://api.apify.com/v2/key-value-stores/ZsOpZgeg7dFS1rgfM/records/LATEST"
+
+// const api_url2 = 
+// "https://api.apify.com/v2/key-value-stores/p3nS2Q9TUn6kUOriJ/records/LATEST"
+
+// function click() {
+//     let dt = fetch(api_url1)
+//     .then (function (response) {
+//         return response.json();
+//     })
+//     .then(function (data){
+//         console.log('success!', data.detail)
+//     });
+//     console.log(dt);
+// }
+
+  
